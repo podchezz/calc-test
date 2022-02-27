c0979547d1accecf1724f27364e2f14c3d1fc61a
podchezz <podchezz@gmail.com>
2022-02-27 23:10:00 +0300

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! It's calculator console application.");
+        System.out.print("Type a: ");
+        double a = scanner.nextDouble();
+        System.out.print("Type b: ");
+        double b = scanner.nextDouble();
+        System.out.print("Type command(1 - sum, 2 - diff, 3 - mult): ");
+        int choice = scanner.nextInt();
+        if (choice == 1) {
+            System.out.println("Sum: " + a + " + " + b + " = " + sum(a, b));
+        } else if (choice == 2) {
+            System.out.println("Diff: " + a + " - " + b + " = " + diff(a, b));
+        } else if (choice == 3) {
+            System.out.println("Mult: " + a + " * " + b + " = " + mult(a, b));
+        } else {
+            System.out.println("Ooops... Something was wrong!");
+            System.out.println("Ooops... Something was wrong!");
+        }
+    }
+
+    public static double sum(double a, double b) {
+        return a + b;
+    }
+
+    public static double diff(double a, double b) {
+        return a - b;
+    }
+
+    public static double mult(double a, double b) {
+        return a * b;
+    }
+}
